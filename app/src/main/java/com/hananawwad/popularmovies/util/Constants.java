75e55881f0eab2c2341417967d89e65e3329978c
HananAwwad <eng.hanan.awwad@gmail.com>
2016-01-10 22:23:06 +0200

    Add generic movie api url Add favorites, trailers and reviews constants

--- app/src/main/java/com/hananawwad/popularmovies/util/Constants.java
+++ app/src/main/java/com/hananawwad/popularmovies/util/Constants.java
@@ -5,8 +5,10 @@ package com.hananawwad.popularmovies.util;
  */
 public class Constants {
 
-    public final static String MOVIE_URL = "http://api.themoviedb.org/3/discover/movie?";
+    public final static String MOVIE_URL = "http://api.themoviedb.org";
     public final static String IMAGE_MOVIE_URL = "http://image.tmdb.org/t/p/";
+    public static final String YOU_TUBE_VIDEO_URL = "http://www.youtube.com/watch?v=";
+    public static final String YOU_TUBE_IMG_URL = "http://img.youtube.com/vi/%s/default.jpg";
     public final static String MODE_VIEW = "mode_view";
     public final static String API_KEY_PARAM = "api_key";
     public final static String SORT_BY_KY_PARAM = "sort_by";
@@ -18,5 +20,11 @@ public class Constants {
     public final static String MOVIE_DETAIL_KEY = "movie_detail";
     public static final String POSTER_IMAGE_KEY = "poster";
     public static final String POSTER_IMAGE_VIEW_KEY = "posterImageView";
+    public static final String FAVORITE = "favorite";
+    public static final String TRAILERS = "trailers";
+    public static final String REVIEWS = "reviews";
+    public static final String MAIN_TRAILER = "main_trailer";
+    public static final String SHOW_FAVORITES = "show_favorites";
+
 
 }
