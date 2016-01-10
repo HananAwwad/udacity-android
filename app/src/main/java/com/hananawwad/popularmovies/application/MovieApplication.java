47a661be66afe6e46b1ab59ebdc8851824a40c77
HananAwwad <eng.hanan.awwad@gmail.com>
2016-01-10 22:23:55 +0200

    Use Stetho library

--- app/src/main/java/com/hananawwad/popularmovies/application/MovieApplication.java
+++ app/src/main/java/com/hananawwad/popularmovies/application/MovieApplication.java
@@ -2,6 +2,8 @@ package com.hananawwad.popularmovies.application;
 
 import android.app.Application;
 
+import com.facebook.stetho.Stetho;
+
 
 /**
  * Created by user on 11/22/2015.
@@ -11,5 +13,6 @@ public class MovieApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Stetho.initializeWithDefaults(this);
     }
 }
